c441f1617be7c9fbecd295bd0856785df6b27c89
B2the52 <112206151+B2the52@users.noreply.github.com>
2023-10-22 20:23:06 -0500

    Added more models not all tho

--- CS_lawn_landscape/models.py
+++ CS_lawn_landscape/models.py
@@ -9,6 +9,17 @@ class RequestContact(models.Model):
     cont_preferredService = models.CharField(max_length=200)
 
 
+class UserReview(models.Model):
+    rev_id = models.ForeignKey('Review', on_delete=models.RESTRICT, null=True)
+
+
+class Review(models.Model):
+    rev_id = models.CharField(max_length=50)
+    rev_rating = models.CharField(max_length=5)
+    rev_comments = models.CharField(max_length=500)
+    serv_id = models.ForeignKey('Service', on_delete=models.RESTRICT, null=True)
+
+
 class Service(models.Model):
     serv_id = models.CharField(max_length=50)
     serv_title = models.CharField(max_length=200)
@@ -21,3 +32,5 @@ class Invoice(models.Model):
     Inv_date = models.CharField(max_length=200)
     Inv_total = models.CharField(max_length=200)
     Inv_description = models.CharField(max_length=200)
+    serv_id = models.ForeignKey('Service', on_delete=models.RESTRICT, null=True)
+
